4a5c42e316adf4ce6e3ff5d2aa92ffdcc27896ca
Avneep Singh <asingh3@covalience.com>
2024-02-21 16:37:42 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:[AuthService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy{
   title = 'Console';
+  constructor(private AS:AuthService) { }
+  ngOnInit(): void {
+    
+  }
+  ngOnDestroy(): void {
+    console.log('clear storage');
+    localStorage.clear();
+  }
 }
